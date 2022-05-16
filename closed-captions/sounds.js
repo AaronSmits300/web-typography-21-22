cffc63c7c2be2f861aecd2113e535a1cfe1c65f0
AaronSmits300 <aaron.smits@hva.nl>
2022-05-16 14:17:08 +0200

    Beschrijvingen toegevoegd

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,22 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	0, //sound0
+	2, //sound1 rijden
+	9, //sound2 Warning
+	12, //sound3 police siren
+	15.3, //sound4 alarm 1
+	16.5, //sound5 alarm 1.5
+	20, //sound6 hallway
+	24.5, //sound7 alarm 2
+	26, //sound8 alarm 2.5
+	31, //sound9 louder alarm
+	32, //sound10 louder alarm.5
+	32.9, //sound11 room change
+	34, //sound12 rising noise background moving
+	77, //sound13 calmness room
+	96, //sound14 schip land
+	98, //sound15 piepen beep lichtjes
+	104, //sound16 telefoon piep
+	114, //sound17 synth zwaar	pulse effect
 ];
